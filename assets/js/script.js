4677ad87046571b03b0d4e9ecf1c7bb2cfbea4c1
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2024-06-04 15:27:35 +0530

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,10 +41,11 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 
 
 /**
- * HEADER 
+ * HEADER  & backtotopbtn
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 let lastScrollpos = 0;
 
@@ -63,11 +64,13 @@ const hideHeader = function () {
 window.addEventListener("scroll",function (){
     if (window.scrollY >= 50 ) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
         hideHeader();
     }else{
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
-}) 
+});
 
 /**
  * HEro slider
